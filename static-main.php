ca534b635734879f4ad7416b2b5df3a4e07ca2ed
Alex Hayes <alex@alution.com>
2011-05-02 14:57:38 +1000

    Allows static-main.php to handle BASE_URL detection when webroot is a symlink.

--- static-main.php
+++ static-main.php
@@ -34,13 +34,14 @@ if (
 	// Define system paths (copied from Core.php)
 	if(!defined('BASE_PATH')) {
 		// Assuming that this file is sapphire/static-main.php we can then determine the base path
-		define('BASE_PATH', rtrim(dirname(dirname(__FILE__))), DIRECTORY_SEPARATOR);
+		define('BASE_PATH', str_replace(DIRECTORY_SEPARATOR, '/', rtrim(dirname(dirname(__FILE__)))), DIRECTORY_SEPARATOR);
 	}
 	if(!defined('BASE_URL')) {
+		$script = str_replace(DIRECTORY_SEPARATOR, '/', realpath($_SERVER['SCRIPT_FILENAME']));
 		// Determine the base URL by comparing SCRIPT_NAME to SCRIPT_FILENAME and getting common elements
 		$path = realpath($_SERVER['SCRIPT_FILENAME']);
-		if(substr($path, 0, strlen(BASE_PATH)) == BASE_PATH) {
-			$urlSegmentToRemove = substr($path, strlen(BASE_PATH));
+		if(substr($script, 0, strlen(BASE_PATH)) == BASE_PATH) {
+			$urlSegmentToRemove = substr($script, strlen(BASE_PATH));
 			if(substr($_SERVER['SCRIPT_NAME'], -strlen($urlSegmentToRemove)) == $urlSegmentToRemove) {
 				$baseURL = substr($_SERVER['SCRIPT_NAME'], 0, -strlen($urlSegmentToRemove));
 				define('BASE_URL', rtrim($baseURL, DIRECTORY_SEPARATOR));
